c11ad1fd0152aa21d9989e7411e4a4e74cd30adc
unknown <eyvaz.aliyev.7740@gmail.com>
2024-06-02 22:17:17 +0400

    Designed

---
+++ assets/index.js
@@ -0,0 +1,37 @@
+var email = prompt('Enter your email address:')
+
+var password = prompt('Enter your password:')
+
+var main = []
+
+main.push(email, password)
+
+console.log(main);
+
+
+var res_email = prompt('Please enter your email for login')
+
+var res_password = prompt('Please enter your password for login')
+
+var main_res = []
+
+main_res.push(res_email, res_password)
+
+console.log(main_res);
+
+if (main[0] === main_res[0] && main[1] === main_res[1]) {
+    console.log('You have successfully entered the system');
+} else {
+    alert('Please check your information');
+}
+
+// for (var i of main) {
+//     if (i.email === res_email && i.password === res_password) {
+//         console.log('You have successfully entered the system');
+//     }else{
+//         alert('Please check your information');
+//     }
+// }
+
+
+
